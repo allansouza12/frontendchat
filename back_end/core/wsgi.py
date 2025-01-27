7b457108337f661f3a479aaf688edeb9979426b6
allansouza12 <allang32166@gmail.com>
2025-01-27 20:57:09 -0300

    corrige wsgi.py para deploy no vercel

--- back_end/core/wsgi.py
+++ back_end/core/wsgi.py
@@ -1,8 +1,4 @@
 import os
-import eventlet.wsgi
-import socketio
-import eventlet
-
 from django.core.wsgi import get_wsgi_application
 from django.contrib.staticfiles.handlers import StaticFilesHandler
 
@@ -12,6 +8,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = StaticFilesHandler(get_wsgi_application())
 application = socketio.WSGIApp(socket, application)
-
-eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 8000)), application)
-
